ce48154decf1e51b07dac5634493bd312ca83e38
Dangchan <dang11230@gmail.com>
2022-05-01 13:47:11 +0900

    Fix: CH2 finished
    Printing hello world with name

--- helloworld/helloworld/ViewController.swift
+++ helloworld/helloworld/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var Hello: UILabel!
+    
+    @IBOutlet var txtname: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+// 액션함수는 일반적으로 클래스 맨 마지막 부분에!
+    
+    
+    @IBAction func btnSend(_ sender: UIButton) {
+        Hello.text = "Hello, " + txtname.text!
+    } // Hello 변수의 text를 바꿈
 }
 
